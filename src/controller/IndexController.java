cca0be8d931c2de2c808c87606fcc26e5c99902e
ricecongee <hjh452128@126.com>
2017-08-24 17:45:09 +0800

    first commit

---
+++ src/controller/IndexController.java
@@ -0,0 +1,15 @@
+package controller;
+ 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+ 
+public class IndexController implements Controller {
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView mav = new ModelAndView("index");
+        mav.addObject("message", "Hello Spring MVC");
+        return mav;
+    }
+}
